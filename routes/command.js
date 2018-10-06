76cdac17e701d2ae5fb51d14a4ef15c3b7193636
Gabriele De Rosa <derosagabriele@outlook.it>
2018-10-06 02:05:38 +0200

    Status

--- routes/command.js
+++ routes/command.js
@@ -43,37 +43,60 @@ module.exports = function(bot, config, request, shell) {
             return ctx.reply('⛔️'); // no auth
         }
 
-        var pc = require('pc');
+        const si = require('systeminformation');
+        const publicIp = require('public-ip');
         const humanizeDuration = require('humanize-duration');
         const pretty = require('prettysize');
         const deasync = require('deasync');
-
-        var status = "💻 *"+pc.hostname().name+"*";
-        status += "\n\n⏳ *"+humanizeDuration(pc.uptime().time * 1000)+"*";
-            var memory = pc.memory();
-        status += "\n\n🧠 Memory";
-        status += "\nFree: *"+pretty(memory.free)+"*";
-        status += "\nTotal: *"+pretty(memory.total)+"*";
-        status += "\n\n🏠 LAN";
-            var nets = pc.networkInterfaces();
-        for(net in nets)
-            if(nets[net][0].address != "" && !nets[net][0].internal)
-                status += "\nIP: *"+nets[net][0].address+"*";
         
-        var sync = true;
-        var public_ip = false;
-        pc.publicIP().then(ip => {
-            public_ip = ip;
-            sync = false;
+        si.getAllData(function(data) {
+
+            // Get public IP
+            var sync = true;
+            var public_ip = false;
+            publicIp.v4().then(ip => {
+                public_ip = ip;
+                sync = false;
+            });
+            while(sync) { deasync.sleep(100); }
+            
+            // Init
+            var status = "";
+            // System informations
+            status += "💻 System Informations";
+            status += "\n_Hostname:_ *"+data.os.hostname+"*";
+            status += "\n_Distro:_ *"+data.os.distro+" "+data.os.release+"*";
+            status += "\n_Architecture:_ *"+data.os.arch+"*";
+            status += "\n_Kernel:_ *"+data.versions.kernel+"*";
+            // Uptime
+            status += "\n\n⏳ Time";
+            status += "\n_Up:_ *"+humanizeDuration(data.time.uptime * 1000)+"*";
+            // Memory
+            status += "\n\n🧠 Memory";
+            status += "\n_Used:_ *"+pretty(data.mem.used)+"*";
+            status += " / _Free:_ *"+pretty(data.mem.free)+"*";
+            status += "\n_Available:_ *"+pretty(data.mem.available)+"*";
+            status += "\n_Total:_ *"+pretty(data.mem.total)+"*";
+            // Temperature
+            status += "\n\n🌡️ Temperature";
+            status += "\n_Main:_ *"+data.temp.main+"°*";
+            status += " / _Max:_ *"+data.temp.max+"°*";
+            for(var i = 0; i < data.temp.cores.length; i++){
+                status += "\n_Core "+(i+1)+":_ *"+data.temp.cores[i]+"°*";
+            }
+            // Network
+            status += "\n\n🌐 Network";
+            status += "\nLocal 🏠";
+            for(var i = 0; i < data.net.length; i++){
+                status += "\n_"+data.net[i].iface+":_ *"+(data.net[i].ip4 || data.net[i].ip6)+"*";
+            }
+            if(public_ip){
+                status += "\nPublic 🌍";
+                status += "\n_IP:_ *"+public_ip+"*";
+            }
+            
+            ctx.reply(status, {disable_web_page_preview: true, parse_mode: 'Markdown'});
         });
-        while(sync) { deasync.sleep(100); }
-
-        if(public_ip){
-            status += "\n\n🌍 Network";
-            status += "\nIP: *"+public_ip+"*";
-        }
-        
-        ctx.reply(status, {disable_web_page_preview: true, parse_mode: 'Markdown'});
 
     }
     bot.command('status', status);
