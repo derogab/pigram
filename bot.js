482a1e98c10e00d6cb9d41b8648d32683aab1dc2
Gabriele De Rosa <derosagabriele@outlook.it>
2021-10-01 17:31:23 +0200

    Versioning

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@
  *
  * @author:     Gabriele De Rosa [@derogab]
  * @file:       bot.js
- * @version:    1.1.4
+ * @version:    1.1.5
  * @link        GitHub Repo:  https://github.com/derogab/pigram
  */
 
